50f0c4815697252640dee271d9106e2a95c838be
unknown <sainyajay@gmail.com>
2020-06-20 13:45:44 +0600

    moved to master

--- js/plugins.js
+++ js/plugins.js
@@ -63,72 +63,8 @@ const autoCompletejs = new autoComplete({
     result.innerHTML = "No Results";
     document.querySelector("#autoComplete_list").appendChild(result);
   },
-  onSelection: (feedback) => {
-    const selectedObj = feedback.selection.value;
-    // Render selected choice to selection div
-    document.querySelector(".selection").innerHTML = JSON.stringify(
-      selectedObj,
-      null,
-      2
-    );
-    // Clear Input
-    document.querySelector("#autoComplete").value = "";
-    // Change placeholder with the selected value
-    // document
-    //   .querySelector("#autoComplete")
-    //   .setAttribute("placeholder", selectedObj.name);
-    // Concole log autoComplete data feedback
-    console.log(feedback);
-    console.log(getRating(selectedObj.type, selectedObj.origin));
-  },
+  onSelection: onItemSelected,
 });
-
-function getRating(type, origin, manufacturedIn) {
-  if (!origin) {
-    return Color.BURGUNDY;
-  }
-
-  if (type.toLowerCase() === Type.COMPANY.toLowerCase()) {
-    if (origin.toLowerCase() === Country.INDIA.toLowerCase()) {
-      return Color.GREEN;
-    } else if (origin.toLowerCase() === Country.CHINA.toLowerCase()) {
-      return Color.RED;
-    } else {
-      return Color.YELLOW;
-    }
-  }
-
-  if (type.toLowerCase() === Type.PRODUCT.toLowerCase()) {
-    if (
-      origin.toLowerCase() === Country.INDIA.toLowerCase() &&
-      manufacturedIn === Country.INDIA.toLowerCase()
-    ) {
-      return Color.GREEN;
-    } else if (
-      origin.toLowerCase() === Country.CHINA.toLowerCase() &&
-      manufacturedIn === Country.CHINA.toLowerCase()
-    ) {
-      return Color.RED;
-    } else if (
-      origin.toLowerCase() === Country.CHINA.toLowerCase() ||
-      manufacturedIn === Country.CHINA.toLowerCase()
-    ) {
-      return Color.ORANGE;
-    } else {
-      return Color.YELLOW;
-    }
-  }
-
-  if (type.toLowerCase() === Type.APP.toLowerCase()) {
-    if (origin.toLowerCase() === Country.INDIA.toLowerCase()) {
-      return Color.GREEN;
-    } else if (origin.toLowerCase() === Country.CHINA.toLowerCase()) {
-      return Color.RED;
-    } else {
-      return Color.YELLOW;
-    }
-  }
-}
 
 // Toggle results list and other elements
 const action = function (action) {
