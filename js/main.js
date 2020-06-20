26ba528a0c095b1c999b624e956a6ea8145a30a4
avinashpipliyacode <avinashpipliya@gmail.com>
2020-06-20 22:25:56 +0530

    gauge added with ui

--- js/main.js
+++ js/main.js
@@ -1,91 +1,21 @@
-var opts = {
+
+function setGaugeVaue(value) {
+
+
+  var opts = {
     // options here
-};
-var target = document.getElementById('demo'); 
-var demoGauge = new Gauge(target).setOptions(opts);
+  };
+  var target = document.getElementById('demo');
+  var demoGauge = new Gauge(target).setOptions(opts);
 
-document.getElementById("preview-textfield").className = "preview-textfield";
-demoGauge.setTextField(document.getElementById("preview-textfield"));
+  document.getElementById("preview-textfield").className = "preview-textfield";
+  //demoGauge.setTextField(document.getElementById("preview-textfield"));
 
 
-// gauge.maxValue = 3000;
-// gauge.setMinValue(0); 
-// gauge.set(1250);
-
-// gauge.animationSpeed = 32
-
-// var opts = {
-
-//     // color configs
-//     colorStart: "#6fadcf",
-//     colorStop: void 0,
-//     gradientType: 0,
-//     strokeColor: "#e0e0e0",
-//     generateGradient: true,
-//     percentColors: [[0.0, "#a9d70b" ], [0.50, "#f9c802"], [1.0, "#ff0000"]],
-
-//     // customize pointer
-//     pointer: {
-//       length: 0.8,
-//       strokeWidth: 0.035,
-//       iconScale: 1.0
-//     },
-
-//     // static labels
-//     staticLabels: {
-//       font: "10px sans-serif",
-//       labels: [200, 500, 2100, 2800],
-//       fractionDigits: 0
-//     },
-
-//     // static zones
-//     staticZones: [
-//       {strokeStyle: "#F03E3E", min: 0, max: 200},
-//       {strokeStyle: "#FFDD00", min: 200, max: 500},
-//       {strokeStyle: "#30B32D", min: 500, max: 2100},
-//       {strokeStyle: "#FFDD00", min: 2100, max: 2800},
-//       {strokeStyle: "#F03E3E", min: 2800, max: 3000}
-//     ],
-
-//     // render ticks
-//     renderTicks: {
-//       divisions: 5,
-//       divWidth: 1.1,
-//       divLength: 0.7,
-//       divColor: '#333333',
-//       subDivisions: 3,
-//       subLength: 0.5,
-//       subWidth: 0.6,
-//       subColor: '#666666'
-//     },
-
-//     // the span of the gauge arc
-//     angle: 0.15,
-
-//     // line thickness
-//     lineWidth: 0.44,
-
-//     // radius scale
-//     radiusScale: 1.0,
-
-//     // font size
-//     fontSize: 40,
-
-//     // if false, max value increases automatically if value > maxValue
-//     limitMax: false,
-
-//     // if true, the min value of the gauge will be fixed
-//     limitMin: false,
-
-//     // High resolution support
-//     highDpiSupport: true
-
-// };
-
-var opts = {
-    angle: -0.25,
-    lineWidth: 0.2,
-    radiusScale:0.9,
+  var opts = {
+    angle: 0,
+    lineWidth: 0.5,
+    radiusScale: 0.9,
     pointer: {
       length: 0.6,
       strokeWidth: 0.05,
@@ -93,27 +23,56 @@ var opts = {
     },
     staticLabels: {
       font: "10px sans-serif",
-      labels: [200, 500, 2100, 2800],
+      labels: [],
       fractionDigits: 0
     },
     staticZones: [
-        {strokeStyle: "#F03E3E", min: 100, max: 500}, // Red from 100 to 130
-        {strokeStyle: "#FFDD00", min: 500, max: 1500}, // Yellow
-        {strokeStyle: "#30B32D", min: 1500, max: 2800}, // Green
-        // {strokeStyle: "#FFDD00", min: 220, max: 260}, // Yellow
-        // {strokeStyle: "#F03E3E", min: 260, max: 300}  // Red
-     ],
+      { strokeStyle: "#F03E3E", min: 0, max: 500 }, // Red from 100 to 130
+      { strokeStyle: "#800020", min: 500, max: 1000 }, // BURGUNDY
+      { strokeStyle: "#FFA500", min: 1000, max: 1500 }, // Orange
+      { strokeStyle: "#FFDD00", min: 1500, max: 2000 }, // Yellow
+      { strokeStyle: "#30B32D", min: 2000, max: 3000 }, // Green
+      // {strokeStyle: "#FFDD00", min: 220, max: 260}, // 
+      // {strokeStyle: "#F03E3E", min: 260, max: 300}  // Red
+    ],
     limitMax: false,
     limitMin: false,
     highDpiSupport: true
   };
   demoGauge.setOptions(opts);
-  demoGauge.setTextField(document.getElementById("preview-textfield"));
+ // demoGauge.setTextField(document.getElementById("preview-textfield"));
   demoGauge.minValue = 0;
   demoGauge.maxValue = 3000;
-  demoGauge.set(1244);
+  //demoGauge.set(1844);
+var gaugeText = ' - '
+  switch (value) {
 
-  
+    case Color.RED:
+      demoGauge.set(200);
+      gaugeText = "No"
+      break;
+
+    case Color.GREEN:
+      demoGauge.set(2544);
+      gaugeText = "Yes"
+      break;
+    case Color.YELLOW:
+      demoGauge.set(1844);
+      gaugeText = "May be"
+      break;
+    case Color.BURGUNDY:
+      demoGauge.set(500);
+      gaugeText = "May be"
+      break;
+    case Color.ORANGE:
+      demoGauge.set(1000);
+      gaugeText = "May be"
+      break;
+  }
+  document.getElementById("preview-textfield").innerHTML=gaugeText;
+}
+
+
 function onItemSelected(feedback) {
   const selectedObj = feedback.selection.value;
   // Render selected choice to selection div
@@ -128,9 +87,10 @@ function onItemSelected(feedback) {
   // document
   //   .querySelector("#autoComplete")
   //   .setAttribute("placeholder", selectedObj.name);
-  // Concole log autoComplete data feedback
+  // Concole log autoComplete data feedback;
   console.log(feedback);
-  console.log(getRating(selectedObj.type, selectedObj.origin));
+  console.log("getRating", getRating(selectedObj.type, selectedObj.origin));
+  setGaugeVaue(getRating(selectedObj.type, selectedObj.origin));
 }
 
 function getRating(type, origin, manufacturedIn) {
