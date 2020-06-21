d39a8772358d9452a67fb8dbc07c01d230ba1acd
Ankit Mishra <ankit.mishra@confirmit.com>
2020-06-21 19:08:25 +0200

    fixed firefox styling, fix service worker offline

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-console.log("Hello!!");
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    }).catch(function() {
+      // If both fail, show a generic fallback:
+      return caches.match('/swadeshi/404.html');
+      // However, in reality you'd have many different
+      // fallbacks, depending on URL & headers.
+      // Eg, a fallback silhouette image for avatars.
+    })
+  );
+});
