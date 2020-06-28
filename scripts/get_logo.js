0ebfe68bdd3a53f3132f82e063819b4c0c394f1f
Ankit Mishra <ankit.mishra@confirmit.com>
2020-06-28 20:12:02 +0200

    cleanup, add script

---
+++ scripts/get_logo.js
@@ -0,0 +1,46 @@
+// Logo fetch script -
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+let randomDelay = () =>
+  new Promise((resolve) => setTimeout(resolve, getRandomArbitrary(500, 1000)));
+
+let calc = async (i) => {
+  await randomDelay();
+  console.log("Fetch.", i.name);
+  const resp = await fetch(
+    "https://autocomplete.clearbit.com/v1/companies/suggest?query=" + i.name,
+    {
+      referrer: "https://clearbit.com/logo",
+      referrerPolicy: "unsafe-url",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      credentials: "omit",
+    }
+  ).then((res) => res.json());
+  //console.log(resp);
+  const match = resp.find(
+    (resp) => resp.name.toLowerCase() === i.name.toLowerCase()
+  );
+  if (match) {
+    //icons.add(match);
+    i.value = match;
+  } else {
+    console.log("No MATCH :", i.name);
+  }
+  return i;
+};
+let results = [];
+
+let asyncFunc = async () => {
+  // const unresolvedPromises = db.map(i=>calc(i));
+  for (let item of db) {
+    const result = await calc(item);
+    //console.log(result);
+    results.push(result);
+  }
+  //results = await Promise.all(unresolvedPromises);
+};
+asyncFunc();
