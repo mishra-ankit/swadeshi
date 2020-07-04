398531903d09163df5113523e43af45b26465068
unknown <sainyajay@gmail.com>
2020-07-04 22:43:11 +0600

    apps alternatives

--- js/alternatives.js
+++ js/alternatives.js
@@ -192,6 +192,426 @@ const chineseProducts = {
 };
 
 const indianProducts = {
+  social: [
+    {
+      name: "Roposo",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/roposo-watch-make-fun-videos/id950273328",
+      android:
+        "https://play.google.com/store/apps/details?id=com.roposo.android",
+      indian: true,
+    },
+    {
+      name: "Chingari",
+      description: "",
+      ios: "https://apps.apple.com/in/app/chingari/id1450361582",
+      android:
+        "https://play.google.com/store/apps/details?id=io.chingari.app&hl=en_IN",
+      indian: true,
+    },
+    {
+      name: "Pixalive",
+      description: "",
+      ios: "https://apps.apple.com/in/app/pixalive/id1491966281",
+      android:
+        "https://play.google.com/store/apps/details?id=com.pixalive&hl=en",
+      indian: true,
+    },
+    {
+      name: "Bolo Indya",
+      description: "",
+      ios: "",
+      android:
+        "https://play.google.com/store/apps/details?id=com.boloindya.boloindya",
+      indian: true,
+    },
+    {
+      name: "ShareChat",
+      description: "",
+      ios: "https://apps.apple.com/in/app/sharechat-videos-status/id1440640105",
+      android:
+        "https://play.google.com/store/apps/details?id=in.mohalla.sharechat",
+      indian: true,
+    },
+  ],
+  office: [],
+  chatting: [
+    {
+      name: "JioChat",
+      description: "",
+      ios: "https://apps.apple.com/in/app/jiochat-video-messenger/id981918884",
+      android:
+        "https://play.google.com/store/apps/details?id=com.jiochat.jiochatapp",
+      india: true,
+    },
+    {
+      name: "Pixalive",
+      description: "",
+      ios: "https://apps.apple.com/in/app/pixalive/id1491966281",
+      android:
+        "https://play.google.com/store/apps/details?id=com.pixalive&hl=en",
+      indian: true,
+    },
+    {
+      name: "ShareChat",
+      description: "",
+      ios: "https://apps.apple.com/in/app/sharechat-videos-status/id1440640105",
+      android:
+        "https://play.google.com/store/apps/details?id=in.mohalla.sharechat",
+      indian: true,
+    },
+    {
+      name: "Hike",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/hike-sticker-chat-hikemoji/id568038211",
+      android:
+        "https://play.google.com/store/apps/details?id=com.hike.chat.stickers",
+      indian: true,
+    },
+  ],
+  music: [
+    {
+      name: "Gaana",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/gaana-music-songs-podcasts/id585270521",
+      android: "https://play.google.com/store/apps/details?id=com.gaana",
+      india: true,
+    },
+    {
+      name: "Jio Saavn",
+      description: "",
+      ios: "https://apps.apple.com/in/app/jiosaavn-music-podcasts/id441813332",
+      android:
+        "https://play.google.com/store/apps/details?id=com.jio.media.jiobeats",
+      indian: true,
+    },
+    {
+      name: "Wynk",
+      description: "",
+      ios:
+        "https://apps.apple.com/lk/app/wynk-music-songs-downloads/id845083955",
+      android:
+        "https://play.google.com/store/apps/details?id=com.bsbportal.music",
+      indian: true,
+    },
+  ],
+  news: [
+    {
+      name: "Inshorts",
+      description: "",
+      ios: "https://apps.apple.com/us/app/inshorts/id892146527",
+      android: "https://play.google.com/store/apps/details?id=com.nis.app",
+      india: true,
+    },
+    {
+      name: "DailyHunt",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/dailyhunt-formerly-newshunt/id338525188",
+      android: "https://play.google.com/store/apps/details?id=com.eterno",
+      indian: true,
+    },
+  ],
+  shopping: [
+    {
+      name: "Tata CLiQ",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/tata-cliq-online-shopping-app/id1101619385",
+      android: "https://play.google.com/store/apps/details?id=com.tul.tatacliq",
+      indian: true,
+    },
+    {
+      name: "Flipkart",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/flipkart-online-shopping-app/id742044692",
+      android:
+        "https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "Snapdeal",
+      description: "",
+      ios: "https://play.google.com/store/apps/details?id=com.snapdeal.main",
+      android:
+        "https://play.google.com/store/apps/details?id=com.snapdeal.main",
+      india: true,
+    },
+    {
+      name: "PayTM",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/mobile-recharge-bill-payments/id473941634",
+      android: "https://play.google.com/store/apps/details?id=net.one97.paytm",
+      india: true,
+    },
+    {
+      name: "Myntra",
+      description: "",
+      ios:
+        "https://apps.apple.com/al/app/myntra-fashion-shopping-app/id907394059",
+      android:
+        "https://play.google.com/store/apps/details?id=com.myntra.android&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "IndiaMart",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/indiamart-buy-sell-products/id668561641",
+      android:
+        "https://play.google.com/store/apps/details?id=com.indiamart.m&hl=en_IN",
+      indian: true,
+    },
+    {
+      name: "Shopclues",
+      description: "",
+      ios: "https://apps.apple.com/il/app/shopclues/id994486085",
+      android:
+        "https://play.google.com/store/apps/details?id=com.shopclues&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "Udaan",
+      description: "",
+      ios: "https://apps.apple.com/in/app/udaan/id1224098850",
+      android:
+        "https://play.google.com/store/apps/details?id=com.udaan.android&hl=en_IN",
+      indian: true,
+    },
+    {
+      name: "Ajio",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/ajio-online-shopping-app/id1113425372",
+      android: "https://play.google.com/store/search?q=ajio&c=apps&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "Limeroad",
+      description: "",
+      ios: "https://apps.apple.com/jo/app/limeroad-shopping/id890803320",
+      android:
+        "https://play.google.com/store/apps/details?id=com.shopping.limeroad&hl=en_IN",
+      indian: true,
+    },
+  ],
+  browsers: [
+    {
+      name: "Jio Browser",
+      description: "",
+      ios: "",
+      android:
+        "https://play.google.com/store/apps/details?id=com.jio.web&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "Bharat Browser",
+      description: "",
+      ios: "",
+      android:
+        "https://play.google.com/store/apps/details?id=com.bharat.browser",
+      indian: true,
+    },
+    {
+      name: "Epic Browser",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/epic-privacy-browser-w-vpn/id1502032811",
+      android:
+        "https://play.google.com/store/apps/details?id=com.epic.browser&hl=en_IN",
+      indian: true,
+    },
+  ],
+  security: [
+    {
+      name: "JioSecurity",
+      description: "",
+      ios: "https://apps.apple.com/in/app/jiosecurity/id1140892768",
+      android:
+        "https://play.google.com/store/apps/details?id=com.reliancejio.mobilesecurity&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "QuickHeal",
+      description: "",
+      ios: "https://apps.apple.com/in/app/quick-heal-optimizer/id1025007234",
+      android:
+        "https://play.google.com/store/apps/details?id=com.quickheal.platform&hl=en_IN",
+      indian: true,
+    },
+  ],
+  finance: [
+    {
+      name: "BHIM",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/bhim-making-india-cashless/id1200315258",
+      android:
+        "https://play.google.com/store/apps/details?id=in.org.npci.upiapp",
+      india: true,
+    },
+    {
+      name: "Mobikwik",
+      description: "",
+      ios:
+        "https://apps.apple.com/jo/app/mobikwik-recharge-bill-pay/id600002523",
+      android: "https://play.google.com/store/apps/details?id=com.mobikwik_new",
+      india: true,
+    },
+    {
+      name: "PayTM",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/mobile-recharge-bill-payments/id473941634",
+      android: "https://play.google.com/store/apps/details?id=net.one97.paytm",
+      india: true,
+    },
+    {
+      name: "PhonePe",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/phonepe-indias-payments-app/id1170055821",
+      android: "https://play.google.com/store/apps/details?id=com.phonepe.app",
+      india: true,
+    },
+  ],
+  videoCalling: [
+    {
+      name: "Say Namaste",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/say-namaste-video-conferencing/id1508559464",
+      android:
+        "https://play.google.com/store/apps/details?id=com.inscripts.namaste",
+      india: true,
+    },
+  ],
+  utility: [],
+  games: [
+    {
+      name: "Ludo King",
+      description: "",
+      ios: "https://apps.apple.com/jm/app/ludo-king/id993090598",
+      android:
+        "https://play.google.com/store/apps/details?id=com.ludo.king&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "Sudoku King",
+      description: "",
+      ios: "https://apps.apple.com/us/app/sudoku-king/id1498907458",
+      android:
+        "https://play.google.com/store/apps/details?id=com.gametion.sudokuking",
+      india: true,
+    },
+    {
+      name: "Carrom King",
+      description: "",
+      ios: "https://apps.apple.com/in/app/carrom-king/id1227693816",
+      android:
+        "https://play.google.com/store/apps/details?id=com.git.carromking",
+      india: true,
+    },
+    {
+      name: "Guardians of the Skies",
+      description: "",
+      ios: "https://apps.apple.com/in/app/guardians-of-the-skies/id903373261",
+      android:
+        "https://play.google.com/store/apps/details?id=com.threye.iaf.gots&hl=en_IN",
+      indian: true,
+    },
+    {
+      name: "Baahubali The Game",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/baahubali-the-game-official/id1213305979",
+      android:
+        "https://play.google.com/store/apps/details?id=com.bahubali.game&hl=en_IN",
+      indian: true,
+    },
+    {
+      name: "World Cricket Championship",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/world-cricket-championship-2/id1025281689",
+      android:
+        "https://play.google.com/store/apps/details?id=com.nextwave.wcc2&hl=en_IN",
+      indian: true,
+    },
+    {
+      name: "World Table Tennis Champs",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/world-table-tennis-champs/id1386609664",
+      android:
+        "https://play.google.com/store/apps/details?id=com.nextwave.wttcpingpong&hl=en_IN",
+      indian: true,
+    },
+    {
+      name: "Real Carrom",
+      description: "",
+      ios: "https://apps.apple.com/us/app/real-carrom/id1105859833",
+      android:
+        "https://play.google.com/store/apps/details?id=com.nextwave.realcarrom",
+      indian: true,
+    },
+  ],
+  fileSharing: [
+    {
+      name: "JioSwitch",
+      description: "",
+      ios:
+        "https://apps.apple.com/in/app/jioswitch-transfer-share-files/id921382666",
+      android:
+        "https://play.google.com/store/apps/details?id=com.reliance.jio.jioswitch&hl=en_IN",
+      india: true,
+    },
+  ],
+  videoEditing: [],
+  photoEditing: [
+    {
+      name: "Indian Selfie Camera",
+      description: "",
+      ios: "",
+      android:
+        "https://play.google.com/store/apps/details?id=com.appbreakers.selfiecamera&hl=en_IN",
+      india: true,
+    },
+  ],
+  scanning: [
+    {
+      name: "Kaagaz",
+      description: "",
+      ios: "",
+      android:
+        "https://play.google.com/store/apps/details?id=kaagaz.scanner.docs.pdf&hl=en_IN",
+      india: true,
+    },
+  ],
+  mail: [
+    {
+      name: "Zoho Mail",
+      description: "",
+      ios:
+        "https://apps.apple.com/us/app/zoho-mail-email-and-calendar/id909262651",
+      android:
+        "https://play.google.com/store/apps/details?id=com.zoho.mail&hl=en_IN",
+      india: true,
+    },
+    {
+      name: "Data Mail",
+      description: "",
+      ios: "https://apps.apple.com/us/app/datamail/id1149220951?ls=1",
+      android:
+        "https://play.google.com/store/apps/details?id=com.datainfosys.datamail",
+      india: true,
+    },
+  ],
   tv: [
     {
       name: "Micromax",
